406a92d95603bea88dcf16f95d37843a537fb2a8
cbushell <c.bushell@gmail.com>
2011-03-17 11:50:35 +0000

    use dom for data model

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,29 +1,12 @@
 $(document).ready(function() {
-  var demoSeries = [
-    { url: "http://localhost:9292/demo/0.csv",
-      legend: "Series 0" },
-    { url: "http://localhost:9292/demo/1.csv",
-      legend: "Series 1" },
-    { url: "http://localhost:9292/demo/2.csv",
-      legend: "Series 2" },
-    { url: "http://localhost:9292/demo/3.csv",
-      legend: "Series 3" },
-    { url: "http://localhost:9292/demo/4.csv",
-      legend: "Series 4" }
-  ];
+  var series = [];
 
-  $.each(demoSeries, function(index, src) {
-    var markup =
-            "<div>" +
-              "<input type='text' value='{url}' class='url' />" +
-              "<input type='text' value='{legend}' class='legend' />" +
-              "<a href='#' class='remove'>Remove</a>" +
-            "</div>";
-
-    markup = markup.replace("{url}", src.url);
-    markup = markup.replace("{legend}", src.legend);
-    $("#series").append($(markup));
+  $("#series div").each(function(index) {
+    var url = $(this).find(".url").val();
+    var legend = $(this).find(".legend").val();
+    series.push({url: url, legend: legend});
   });
+
 
   $("#chart").bind("click", function(){
     $("#series").toggle();
@@ -31,10 +14,10 @@ $(document).ready(function() {
 
   $("#series a").bind("click", function(){
     $(this).parent().remove();
-    Cruise.refresh(demoSeries);
+    Cruise.refresh(series);
   });
 
-  Cruise.refresh(demoSeries);
+  Cruise.refresh(series);
 }).ajaxStop(
   function(){
     Plotter.plot(Cruise.series)
