242658afde10723fd541a4cb8f9fdd78aebe445d
cbushell <c.bushell@gmail.com>
2011-03-21 15:17:45 +0000

    consistent naming

--- public/javascript/csv-parser.js
+++ public/javascript/csv-parser.js
@@ -1,22 +1,22 @@
 var CSVParser = {
   parse: function(data) {
-    var data_series = [];
+    var dataSeries = [];
     var rows = data.split(/\n/);
 
     $.each(rows, function(row_index, row) {
       if (row_index > 0) {
         var tokens = row.split(",");
-        var build_duration = tokens[1];
-        var build_start_time = Date.parse(tokens[8]);
+        var buildDuration = tokens[1];
+        var buildStartTime = Date.parse(tokens[8]);
 
         var build_status = tokens[3];
 
         if (build_status == 'Passed') {
-          data_series.push([build_start_time.getTime(), build_duration]);
+          dataSeries.push([buildStartTime.getTime(), buildDuration]);
         }
       }
     });
 
-    return data_series;
+    return dataSeries;
   }
 };
--- public/javascript/main.js
+++ public/javascript/main.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
     $("#series").toggle();
   });
 
-  $("#series a").bind("click", function(){
+  $("#series a.add").bind("click", function(){
+    var markup = "<div><input></input><input></input><a></a></div>";
+    $(this).parent().append(markup);
+  });
+
+  $("#series a.remove").bind("click", function(){
     $(this).parent().remove();
     Cruise.refresh();
   });
